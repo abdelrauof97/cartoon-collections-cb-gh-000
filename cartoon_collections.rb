429c96988ab439516f6461b0af25e4cd5a571638
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-01-09 20:54:01 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,14 +1,14 @@
 planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
 dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
-
+short_words = ["puff", "go", "two"]
 def roll_call_dwarves(array)
   array.each_with_index { |x,i| puts "#{i+1}. #{x}" }
 end
 def summon_captain_planet(array)
   array.map { |x| "#{x.capitalize}!" }
 end
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.length > 3 ? return ture : return false
 end
 
 def find_the_cheese# code an argument here
