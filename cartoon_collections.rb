613cd16f6ca877ddade74b5ac5536cf17d0e1d46
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-01-09 20:48:12 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,8 +1,9 @@
 planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+
 def roll_call_dwarves(array)
   array.each_with_index { |x,i| puts "#{i+1}. #{x}" }
 end
-roll_call_dwarves(["Doc", "Dopey", "Bashful", "Grumpy"])
+roll_call_dwarves()
 def summon_captain_planet(array)
   array.map { |x| "#{x}!" }
 end
