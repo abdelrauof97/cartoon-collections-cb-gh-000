b42266a4e7c0b1ab44bf20767535c2b4d79c8f4a
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-01-09 21:14:24 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,8 +17,8 @@ def long_planeteer_calls(array)
 end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.map { |x| 
-  if x == cheese_types.any?
+  cheese_types.map { |x| 
+  if x == array.any?
     true
   else 
     return nil
