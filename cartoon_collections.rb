6279d370ad8d3feeed4ffab8977e2f48443a967a
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-01-09 21:01:59 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,6 @@
 planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
 dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
 short_words = ["puff", "go", "two"]
-cheese_types = ["cheddar", "gouda", "camembert"]
 def roll_call_dwarves(array)
   array.each_with_index { |x,i| puts "#{i+1}. #{x}" }
 end
