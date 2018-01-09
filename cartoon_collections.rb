e49206102df1c57b6c93079aec03d150f559cc32
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-01-09 21:20:03 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,10 +17,10 @@ def long_planeteer_calls(array)
 end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese_types.each { |x| 
-
+  new_array = []
+  if array.any? == "cheddar"
+    puts "hi"
 end
-find_the_cheese(soup)
 
 
 
